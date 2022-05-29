1ee5fb6e791ac7c8e74fc4af0682a48094835ca5
kry <krizsan@gmail.com>
2022-05-29 12:07:12 +0200

    test for merge solution

--- src/test/java/hu/mycompany/machinemanager/service/IntegTest.java
+++ src/test/java/hu/mycompany/machinemanager/service/IntegTest.java
@@ -15,7 +15,11 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 class MachineDay implements Comparable {
 
@@ -79,14 +83,64 @@ class MachineDay implements Comparable {
 class IntegTest {
 
     @Test
+    void date() throws Exception {
+        List<LocalDate> normalList = LocalDate.of(2000, 1, 1)
+            .datesUntil(LocalDate.of(2000, 1, 14))
+            .collect(Collectors.toList());
+
+        Assert.assertEquals(13, normalList.size());
+
+        try {
+            List<LocalDate> invalidList = LocalDate.of(2000, 1, 10)
+                .datesUntil(LocalDate.of(2000, 1, 1))
+                .collect(Collectors.toList());
+            fail("no exception ????");
+        } catch (IllegalArgumentException illegalArgumentException) {
+
+        }
+
+        List<LocalDate> noDayz = LocalDate.of(2000, 1, 1)
+            .datesUntil(LocalDate.of(2000, 1, 1))
+            .collect(Collectors.toList());
+        Assert.assertEquals(0, noDayz.size());
+
+    }
+
+    @Test
     void one() throws Exception {
+        LocalDate from = LocalDate.of(2000, 1, 1);
+        LocalDate inProgressStartedAt = LocalDate.of(1999, 12, 29);
+        LocalDate to = LocalDate.of(2000, 1, 14);
+
+        assert(inProgressStartedAt.toEpochDay() <= from.toEpochDay());
+        assert(from.toEpochDay() <= to.toEpochDay());
+
+        Job jobInprogress = new Job()
+            .estimation(6)
+            .startDate(inProgressStartedAt)
+            .productCount(100).id(123L)
+            .worknumber("job in")
+            .addProduct(new Product().name("productName"));
+
+        List<MachineDay> inProgressDays = new ArrayList<>();
+
+        try {
+            inProgressDays = from
+                .datesUntil(jobInprogress.getStartDate().plusDays(jobInprogress.getEstimation()))
+                .map( date -> new MachineDay(LocalDate.EPOCH, true, jobInprogress.getWorknumber(), jobInprogress.getId()))
+                .collect(Collectors.toList());
+        } catch ( IllegalArgumentException illegalArgumentException ) {
+            System.out.println("nooooo");
+        }
+
+
         Stream<OutOfOrder> streamOut = Stream.of(
             new OutOfOrder().start(LocalDate.of(2000, 1, 2)).end(LocalDate.of(2000, 1, 3)).description("desc"),
             new OutOfOrder().start(LocalDate.of(2000, 1, 10)).end(LocalDate.of(2000, 1, 12)).description("desc")
         );
-        List<MachineDay> free = LocalDate
-            .of(2000, 1, 1)
-            .datesUntil(LocalDate.of(2000, 1, 14))
+
+        List<MachineDay> freeDays = from
+            .datesUntil(to)
             .map(date -> new MachineDay(date, false, "free", null))
             .collect(Collectors.toList());
 
@@ -100,14 +154,17 @@ class IntegTest {
             )
             .collect(Collectors.toList());
 
-        free.removeAll(occupied);
+        freeDays.removeAll(occupied);
 
         Stream<Job> jobs = Stream.of(
             new Job().estimation(3).productCount(100).id(123L).worknumber("job 1").addProduct(new Product().name("productName")),
             new Job().estimation(2).productCount(100).id(123L).worknumber("job 2").addProduct(new Product().name("productName"))
         );
 
-        List<MachineDay> jobMachineDayList = jobs
+        // create MachineDay with invalid date
+        List<MachineDay> jobMachineDayList = new ArrayList<>();
+
+        List<MachineDay> remainingJobDays = jobs
             .flatMap(
                 job ->
                     Stream
@@ -116,21 +173,23 @@ class IntegTest {
             )
             .collect(Collectors.toList());
 
+        jobMachineDayList.addAll(inProgressDays);
+        jobMachineDayList.addAll(remainingJobDays);
+
         List<MachineDay> all = new ArrayList<>();
-        for (int i = 0; i < free.size(); i++) {
+        for (int i = 0; i < freeDays.size(); i++) {
             MachineDay jobMachineDay = null;
             MachineDay freeMachineDay = null;
             if (jobMachineDayList.size() > 0) {
                 jobMachineDay = jobMachineDayList.remove(0);
-                freeMachineDay = free.remove(0);
+                freeMachineDay = freeDays.remove(0);
             } else {
                 break;
             }
 
             all.add(new MachineDay(freeMachineDay.getDate(), true, jobMachineDay.getComment(), jobMachineDay.getJobId()));
         }
-
-        all.addAll(free);
+        all.addAll(freeDays);
         all.addAll(occupied);
         all = all.stream().sorted().collect(Collectors.toList());
         System.out.println(all);
